23e1e92803283f96fd274e17e4f0291b1fa3b0b3
Gregor Aisch <contact@vis4.net>
2012-09-27 21:37:16 +0200

    auto commit

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 git add .
 git commit -a -m 'auto commit'
 git push
-ssh datawrapper@s10.wservices.ch cd /home/datawrapper/blog/wp-content/themes/datawrapper-wordpress/;git pull
+ssh datawrapper@s10.wservices.ch "cd /home/datawrapper/blog/wp-content/themes/datawrapper-wordpress/;git pull"
